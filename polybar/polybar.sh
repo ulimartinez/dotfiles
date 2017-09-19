fb401d9579b7667be8143e80a1e830defbf62e16
Ulises <ulimartinez96@gmail.com>
2017-09-18 20:26:14 -0600

    added handle monitor dissconect

---
+++ polybar/polybar.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+killall -q polybar
+# Wait until the processes have been shut down
+while pgrep -x polybar >/dev/null; do sleep 1; done
+
+for i in $(polybar -m | awk -F: '{print $1}'); do MONITOR=$i polybar custom -c ~/.config/polybar/config & done
