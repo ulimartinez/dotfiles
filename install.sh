5cb687b535cc7269ce8978c10d0742707da81687
Ulises <ulimartinez96@gmail.com>
2017-10-26 11:13:17 -0600

    added sym links in install script

--- install.sh
+++ install.sh
@@ -4,16 +4,30 @@ GREEN=$(tput setaf 2)
 NORMAL=$(tput sgr0)
 
 col=80 # change this to whatever column you want the output to start at
-
+deps=(i3 polybar rofi)
+all=1
+#check if a dependency exists
 check(){
     if hash $@ 2>/dev/null; then
+	echo "checking for $@"
 	printf '%s%*s%s\n'  "$GREEN" $col "[OK]" "$NORMAL"
     else
 	printf '%s%*s%s\n' "$RED" $col "[FAIL]" "$NORMAL"
+	all=0
     fi
 }
-printf '%s\n' "Checking for i3"
-check i3
-printf '%s\n' "Checking for polybar"
-check polybar
+for i in ${deps[*]}; do
+    check $i
+done
+
+if [ $all -eq 0 ]; then
+    echo "Missing dependencies, aborting"
+    exit
+else
+    for f in ${deps[*]}; do
+	ln -sf $(pwd)/$f $HOME/.$f
+    done
+fi
+
+
 #TODO: finish this plz
--- polybar/scripts/polybar.sh
+++ polybar/scripts/polybar.sh
@@ -4,4 +4,4 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -x polybar >/dev/null; do sleep 1; done
 
-for i in $(polybar -m | awk -F: '{print $1}'); do MONITOR=$i polybar custom -c ~/.config/polybar/config & done
+for i in $(polybar -m | awk -F: '{print $1}'); do MONITOR=$i polybar custom -c ~/.polybar/config & done
