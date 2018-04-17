68f055278bc3ca1d31cd9b27a207a2bace630f91
Ulises <ulimartinez96@gmail.com>
2018-04-17 15:21:02 -0700

    added adittional links in install.sh

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ GREEN=$(tput setaf 2)
 NORMAL=$(tput sgr0)
 
 col=80 # change this to whatever column you want the output to start at
-deps=(i3 polybar rofi termite)
+deps=(i3 polybar rofi termite emacs)
 all=1
 #check if a dependency exists
 check(){
@@ -19,15 +19,19 @@ check(){
 for i in ${deps[*]}; do
     check $i
 done
-
+# check if all depenencies are installed
 if [ $all -eq 0 ]; then
     echo "Missing dependencies, aborting"
     exit
 else
+    # make a symlink to home directory for every dependency
+    #TODO: check if we have a config dir for the dependency
     for f in ${deps[*]}; do
 	ln -sf $(pwd)/$f $HOME/.$f
     done
 fi
-
-
+# symlink all the files defined in homeconfigs.txt
+line=$(head -n 1 "homeconfigs.txt")
+for conf in $line:
+	    ln -sf $(pwd)/$conf $HOME/$conf
 #TODO: finish this plz
