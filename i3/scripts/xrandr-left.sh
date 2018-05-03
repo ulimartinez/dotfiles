a4986e3c85d3bbeba9b1c2eb8231c2655f9cbb91
Ulises <ulimartinez96@gmail.com>
2018-05-02 23:35:35 -0600

    added monospace font

--- i3/scripts/xrandr-left.sh
+++ i3/scripts/xrandr-left.sh
@@ -1,10 +1,11 @@
 
-EXT=`xrandr --current | sed 's/^\(.*\) connected.*$/\1/p;d' | grep -v ^eDP | head -n 1`
-INT=`xrandr --current | sed 's/^\(.*\) connected.*$/\1/p;d' | grep -v ^DP | head -n 1`
+EXT=DP-2
+INT=eDP-1
 
 ext_w=`xrandr | sed 's/^'"${EXT}"' [^0-9]* \([0-9]\+\)x.*$/\1/p;d'`
 ext_h=`xrandr | sed 's/^'"${EXT}"' [^0-9]* [0-9]\+x\([0-9]\+\).*$/\1/p;d'`
 int_w=`xrandr | sed 's/^'"${INT}"' [^0-9]* \([0-9]\+\)x.*$/\1/p;d'`
-off_w=`echo $(( ($int_w-$ext_w)/2 )) | sed 's/^-//'`
+int_h=`xrandr | sed 's/^'"${INT}"' [^0-9]* [0-9]\+x\([0-9]\+\).*$/\1/p;d'`
+off_w=`echo $(( ($ext_w)*2 ))`
 
-xrandr --output "${INT}" --auto --pos ${off_w}x${ext_h} --scale 1x1 --output "${EXT}" --auto --scale 2x2 --pos 0x0
+xrandr --output "${INT}" --auto --pos ${off_w}x0  --scale 1x1 --panning ${int_w}x${int_h} --output "${EXT}" --auto --scale 2x2 --pos 0x0 --panning 0x0 --left-of "${INT}"
