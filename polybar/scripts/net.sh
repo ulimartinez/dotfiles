29e0240d04ca959318dd1d40badf3ac593ece629
Ulises <ulimartinez96@gmail.com>
2018-07-09 18:54:40 -0600

    fixed network script

--- polybar/scripts/net.sh
+++ polybar/scripts/net.sh
@@ -1,3 +1,21 @@
 #!/bin/bash
 TYPE=0
-echo ETH
+if [ "$TYPE" -eq "0" ]; then
+    MCH="$(iw dev wlp1s0 link | sed -E 's/.*SSID: (.*)$/\1/;tx;d;:x')"
+else
+    MCH=$(ip addr show dev wlp1s0 | sed -E 's/.*inet (([0-9]*\.){3}[0-9]*).*/\1/;tx;d;:x')
+fi
+EMCH=$(echo $MCH | tr -d '[:blank:]')	  
+if [ -z $EMCH ] && [ -z $(ip addr | grep bond0) ]; then
+    MCH=$(ip addr show dev bond0 | sed -E 's/.*inet (([0-9]*\.){3}[0-9]*).*/\1/;tx;d;:x')
+    EMCH=$(echo $MCH | tr -d '[:blank:]')
+    if [ -z $EMCH ]; then
+	echo ""
+    elif [ "$TYPE" -eq "0" ]; then
+	echo "ETH"
+    else
+	echo $MCH
+    fi
+else
+    echo $MCH
+fi
