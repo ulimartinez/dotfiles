676c79c5b9f92205157cb27ddc96cc4d2385ca84
Ulises <ulimartinez96@gmail.com>
2018-11-08 21:23:12 -0700

    rc fixes

--- i3/scripts/social.sh
+++ i3/scripts/social.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 if [ $(i3-save-tree --workspace 2 | grep "class\": \"^Firefox"|wc -l) -lt 1 ];then
-    i3-msg "workspace 2; append_layout ~/.i3/layouts/social.json; exec firefox messenger.com youtube.com reddit.com"
+    i3-msg "workspace 2; append_layout ~/.i3/layouts/social.json; exec firefox messenger.com youtube.com reddit.com web.whatsapp.com"
     #firefox -new-tab facebook.com
 else
     i3-msg "workspace 2"
